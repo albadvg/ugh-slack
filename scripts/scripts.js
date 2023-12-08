9c11250430ef4dc46aee30c1e9b4ab67ac7bc5bf
Alba <albadvg@yahoo.es>
2023-12-08 01:10:21 +0100

    footer menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,20 @@ burguerIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     headerNav.classList.remove('header-nav--open');
 })
+
+//footer menu behaviou (on mobile)
+
+let footerTitles = document.querySelectorAll('.footer-menu-list__title');
+
+footerTitles.forEach(title => title.addEventListener('click', open));
+
+function open (e) {
+    let arrow = this.firstElementChild;
+    let list = this.nextElementSibling;
+    arrow.classList.toggle('footer-menu-list__arrow--open');
+    list.classList.toggle('footer-menu-list-secondary--open');
+}   
+
+// const element = document.getElementById('foo');
+// const styles = window.getComputedStyle(element, '::after');
+// const content = styles.content;
